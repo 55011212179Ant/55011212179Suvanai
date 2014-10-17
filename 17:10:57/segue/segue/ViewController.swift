9879af926b5f767ab98a8b731267738cf20972df
55011212148 <iMac@msu>
2014-10-17 15:24:19 +0700

    17/10/57

---
+++ 17:10:57/segue/segue/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  segue
+//
+//  Created by student on 10/17/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var ColorString=""
+    
+    @IBAction func saveColor(sender: UIBarButtonItem) {
+    }
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    @IBAction func ColorSelectionButton(sender: UIButton) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
